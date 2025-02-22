228931870d5597d774edc5c4a0fa0da31113eb78
nk-31012002 <102040326+nk-31012002@users.noreply.github.com>
2025-02-22 15:58:23 +0530

    second commit

--- Question.java
+++ Question.java
@@ -6,7 +6,16 @@ public class Question {
     private String opt3;
     private String opt4;
     private String ans;
-    
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String ans) {
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.ans = ans;
+    }
     public int getId() {
         return id;
     }
--- QuestionService.java
+++ QuestionService.java
@@ -1,3 +1,21 @@
 public class QuestionService {
     Question[] questions = new Question[5];
+
+    public QuestionService(){
+        questions[0] = new Question(1, "What is the capital of Australia?", "Sydney", "Melbourne", "Canberra", "Brisbane", "Canberra");
+        questions[1] = new Question(2, "Which planet is known as the Red Planet?", "Venus", "Mars", "Jupiter", "Saturn", "Mars");
+        questions[2] = new Question(3, "Who wrote the play Romeo and Juliet?", "Charles Dickens", "William Shakespeare", "Jane Austen", "George Orwell", "William Shakespeare");
+        questions[3] = new Question(4, "What is the chemical symbol for gold?", "Go", "Au", "Ag", "Fe", "Au");
+        questions[4] = new Question(5, "Which country hosted the 2016 Summer Olympics?", "China", "United Kingdom", "Brazil", "Japan", "Brazil");
+
+    }
+
+
+
+    public void displayQuestions(){
+        for(Question q : questions){
+            System.out.println(q.getQuestion());
+        }
+        
+    }
 }
